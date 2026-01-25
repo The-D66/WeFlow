3956989b676ccd81038a407fd2d5784e768c50bb
cc <98377878+hicccc77@users.noreply.github.com>
2026-01-25 15:14:24 +0800

    fix: 一些bug

--- src/pages/SnsPage.tsx
+++ src/pages/SnsPage.tsx
@@ -264,7 +264,7 @@ export default function SnsPage() {
             setHasNewer(false)
             setSelectedUsernames([])
             setSearchKeyword('')
-            setJumpTargetDate(null)
+            setJumpTargetDate(undefined)
             loadContacts()
             loadPosts({ reset: true })
         }
